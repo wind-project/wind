6b607b32f7dff12c7a67cbfd1afe83bdbe993f20
Nikolaos Nikalexis <nikalexis@gmail.com>
2005-08-11 00:52:15 +0000

    Nodes search page - add extra search field.

--- includes/pages/nodes/nodes_search.php
+++ includes/pages/nodes/nodes_search.php
@@ -28,37 +28,44 @@ class nodes_search {
 	}
 	
 	function form_search_nodes() {
-		global $db;
+		global $db, $lang;
 		$form_search_nodes = new form(array('FORM_NAME' => 'form_search_nodes'));
 		$form_search_nodes->db_data('nodes.id, nodes.name, areas.id, regions.id');
 		$form_search_nodes->db_data_enum('areas.id', $db->get("id AS value, name AS output", "areas"));
 		$form_search_nodes->db_data_enum('regions.id', $db->get("id AS value, name AS output", "regions"));
 		array_push($form_search_nodes->data, array('Compare' => 'numeric', 'Field' => 'total_active_peers', 'fullField' => 'total_active_peers'));
 		array_push($form_search_nodes->data, array('Compare' => 'numeric', 'Field' => 'total_active_clients', 'fullField' => 'total_active_clients'));
+		array_push($form_search_nodes->data, array('Field' => 'has_ap', 'fullField' => 'has_ap', 'Type' => 'enum', 'Type_Enums' => array(array('value' => 'Y', 'output' => $lang['yes']), array('value' => 'N', 'output' => $lang['no']))));
 		$form_search_nodes->db_data_search();
 		return $form_search_nodes;
 	}
 
 	function table_nodes() {
-		global $construct, $db, $main;
+		global $construct, $db, $main, $lang;
 		$form_search_nodes = $this->form_search_nodes();
-		$where = $form_search_nodes->db_data_where(array("nodes__name" => "starts_with", "total_active_peers" => 'exclude', "total_active_clients" => 'exclude'));
-		$having = $form_search_nodes->db_data_where(array("nodes__id" => "exclude", "nodes__name" => "exclude", "areas__id" => "exclude", "regions__id" => "exclude"));
+		$where = $form_search_nodes->db_data_where(array("nodes__name" => "starts_with", "total_active_peers" => 'exclude', "total_active_clients" => 'exclude', "has_ap" => 'exclude'));
+		$having = $form_search_nodes->db_data_where(array("nodes__id" => "exclude", "nodes__name" => "exclude", "areas__id" => "exclude", "regions__id" => "exclude", "has_ap" => 'exclude'));
+		if ($form_search_nodes->data[6]['value'] != '') {
+			$having .= ($having!=''?' AND ':'').'total_active_aps '.($form_search_nodes->data[6]['value']=='Y'?'>':'=').' 0';
+		}
 		$table_nodes = new table(array('TABLE_NAME' => 'table_nodes'));
 		$table_nodes->db_data(
-			'nodes.id, nodes.name AS nodes__name, areas.name AS areas__name, COUNT(l1.id) AS total_active_peers, COUNT(cl.id) AS total_active_clients',
+			'nodes.id, nodes.name AS nodes__name, areas.name AS areas__name, COUNT(DISTINCT l2.id) AS total_active_p2p, COUNT(DISTINCT aps.id) AS total_active_aps, COUNT(DISTINCT l1.id) AS total_active_peers, COUNT(DISTINCT cl.id) AS total_active_clients',
 			'nodes
 			LEFT JOIN areas ON nodes.area_id = areas.id
 			LEFT JOIN regions ON areas.region_id = regions.id
 			LEFT JOIN links AS l1 ON nodes.id = l1.node_id
 			LEFT JOIN links AS l2 ON l1.type != "ap"
-			LEFT JOIN links AS cl ON l1.type = "ap" AND cl.status = "active" AND cl.type = "client" AND cl.peer_ap_id = l1.id
-			',
+			LEFT JOIN links AS aps ON nodes.id = aps.node_id AND aps.type = "ap" AND aps.status = "active"
+			LEFT JOIN links AS cl ON l1.type = "ap" AND cl.status = "active" AND cl.type = "client" AND cl.peer_ap_id = l1.id ' .
+			'INNER JOIN users_nodes ON nodes.id = users_nodes.node_id ' .
+			'LEFT JOIN users ON users.id = users_nodes.user_id',
 			"(l1.type IS NULL OR (" .
 			"(l1.type = 'p2p' AND l2.type = 'p2p' AND l1.peer_node_id = l2.node_id AND l2.peer_node_id = l1.node_id) OR " .
 			"(l1.type = 'ap') OR " .
 			"(l1.type = 'client' AND l2.type = 'ap' AND l1.peer_ap_id = l2.id)) AND " .
-			"(l1.status = 'active' AND (l2.status = 'active' OR l1.type = 'ap')))".
+			"(l1.status = 'active' AND (l2.status = 'active' OR l1.type = 'ap'))) " .
+			"AND users.status = 'activated'".
 			($where!=''?' AND ('.$where.')':""),
 			'nodes.id'.
 			($having!=''?' HAVING ('.$having.')':""),
@@ -67,11 +74,12 @@ class nodes_search {
 		for($i=1;$i<count($table_nodes->data);$i++) {
 			if (isset($table_nodes->data[$i])) {
 				$table_nodes->data[$i]['nodes__name'] .= " (#".$table_nodes->data[$i]['id'].")";
+				$table_nodes->data[$i]['total_active_peers'] = $table_nodes->data[$i]['total_active_p2p'].($table_nodes->data[$i]['total_active_aps']>0?" (+".$table_nodes->data[$i]['total_active_aps']." ".$lang['aps'].")":"");
 				$table_nodes->info['EDIT'][$i] = makelink(array("page" => "nodes", "node" => $table_nodes->data[$i]['id']));
 			}
 		}
 		$table_nodes->info['EDIT_COLUMN'] = 'nodes__name';
-		$table_nodes->db_data_remove('id');
+		$table_nodes->db_data_remove('id', 'total_active_p2p', 'total_active_aps');
 		return $table_nodes;
 	}
 	
