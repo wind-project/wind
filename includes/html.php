3ea110e8838a94fa4beb30e7ebcfa84f29a80638
Faidon Liambotis <paravoid@cube.gr>
2005-12-12 03:06:50 +0000

    Remove unused format_html function.

--- includes/html.php
+++ includes/html.php
@@ -27,39 +27,10 @@ class html {
 	var $tpl;
 	var $head;
 	var $body;
-	var $do_format = FALSE;
 	
 	function html() {
 		$this->head = new head;
 		$this->body = new body;		
-	}
-	
-	function format_html($html) {
-		$spliter = "  ";
-		$offset = 0;
-		$tabs = 0;
-		while (($pos = strpos($html, "<", $offset)) !== false) {
-			if ($pos-$offset > 0) $ret .= substr($html, $offset, $pos-$offset)."\n".str_repeat($spliter, $tabs);
-			$offset = strpos($html, ">", $pos);
-			if (substr($html, $pos+1, 1) == "/") {
-				$offset++;
-				$tabs--;
-				$ret = substr($ret, 0, -strlen($spliter));
-				$ret .= substr($html, $pos, $offset-$pos)."\n".str_repeat($spliter, $tabs);				
-			} else {
-				if (substr($html, $pos+1, 1) == "!" || substr($html, $pos+1, 1) == "?" || substr($html, $offset-1, 1) == "/") {
-					$offset++;
-					$ret .= substr($html, $pos, $offset-$pos)."\n".str_repeat($spliter, $tabs);
-				} else {
-					$offset++;
-					$tabs++;
-					$ret .= substr($html, $pos, $offset-$pos)."\n".str_repeat($spliter, $tabs);				
-				}
-			}
-		}
-		if ($pos-$offset > 0) $ret .= substr($html, $offset, $pos-$offset)."\n".str_repeat($spliter, $tabs);
-		if ($ret == "") $ret = $html;
-		return $ret;
 	}
 	
 	function output() {
@@ -67,7 +38,7 @@ class html {
 		$this->tpl['body'] = $this->body->output();
 		$this->tpl['body_tags'] = $this->body->tags;
 		$ret = template($this->tpl, __FILE__);
-		return ($this->do_format?$this->format_html($ret):$ret);
+		return $ret;
 	}
 	
 }
