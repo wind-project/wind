5b4081ac6b1964e45ccc57c0061891fe55e77550
Yiannis Samouhos <ysamouhos@users.noreply.github.com>
2014-05-25 23:35:11 +0300

    NodeChanges Additions

--- globals/language/english.php
+++ globals/language/english.php
@@ -82,10 +82,11 @@ $lang = array(
 	'services_categories' => "Services categories",
 	'services_categories_add' => "Add service category",
 	'services_categories_edit' => "Edit service category",
+	'nodesettingschanges_add' => "Add Change Log for this Node",
+	'nodesettingschanges' => "ChangeLog",
 	'services_edit' => "Edit service",
 	'services_add' => "Add service",
 	'services' => "Services",
-	
 	'user_add' => "New user",
 	'user_edit' => "User profile",
 	'node' => "Node",
@@ -377,6 +378,15 @@ $lang = array(
 		'nodes_services__protocol-tcp' => 'TCP',
 		'nodes_services__protocol-udp' => 'UDP',
 		'nodes_services__port' => 'Port',
+		
+		'nodesettingschanges__id' => 'id',
+	     	'nodesettingschanges__node_id' => 'nodeid',
+		'nodesettingschanges__uid' => 'uid',
+		'nodesettingschanges__changemenu' => 'Section Changed',
+		'nodesettingschanges__changemade' => 'Last Change',
+		'nodesettingschanges__reason' => 'Change Reason',
+		'nodesettingschanges__comment' => 'Comment',
+		'nodesettingschanges__dateline' => 'Date and Time',
 		
 		'photos__date_in' => 'Date',
 		'photos__view_point' => 'Aspect',
