42b7f6604e6533400988d8ff36785800e8431119
Yiannis Samouhos <ysamouhos@users.noreply.github.com>
2014-05-26 01:40:53 +0300

    revert to nodes_services__node_id on success

--- includes/pages/node_editor/node_editor_services.php
+++ includes/pages/node_editor/node_editor_services.php
@@ -75,7 +75,7 @@ class node_editor_services {
 								"nodes_services", "id", $service);
 		
 		if ($ret) {
-			$main->message->set_fromlang('info', 'insert_success', make_ref('/node_editor', array("node" => $_POST['nodes__node_id'])));
+			$main->message->set_fromlang('info', 'insert_success', make_ref('/node_editor', array("node" => $_POST['nodes_services__node_id'])));
 		} else {
 			$main->message->set_fromlang('error', 'generic');		
 		}
